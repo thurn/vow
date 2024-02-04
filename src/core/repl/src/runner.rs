74974e9ffe6ce9ebc912a519cb5cdf3751511067
Derek Thurn <derek@fake.email>
2024-02-04 07:39:46 -0800

    add more stdenv

--- src/core/repl/src/runner.rs
+++ src/core/repl/src/runner.rs
@@ -16,9 +16,10 @@
 
 use std::collections::HashMap;
 use std::f64::consts;
-use std::sync::{Arc, Mutex};
+use std::iter;
 
 use reedline::{DefaultPrompt, Reedline, Signal};
+use slotmap::{DefaultKey, SlotMap};
 
 type Symbol = String;
 type Number = f64;
@@ -34,11 +35,12 @@ enum Atom {
 
 type List = Vec<Exp>;
 
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug)]
 enum Exp {
     Atom(Atom),
     List(List),
-    Function(fn(List) -> Exp),
+    Function(fn(&mut EnvTree, List) -> Exp),
+    Procedure(Box<Procedure>),
 }
 
 impl Exp {
@@ -46,10 +48,28 @@ impl Exp {
         Self::Atom(Atom::Number(number))
     }
 
+    fn bool(b: Bool) -> Self {
+        Self::Atom(Atom::Bool(b))
+    }
+
     fn as_symbol(&self) -> Symbol {
         match self {
             Exp::Atom(Atom::Symbol(s)) => s.clone(),
             _ => panic!("Expected symbol!"),
+        }
+    }
+
+    fn as_exp_list(&self) -> Vec<Exp> {
+        match self {
+            Exp::List(list) => list.clone(),
+            _ => panic!("Expected list"),
+        }
+    }
+
+    fn as_symbol_list(&self) -> Vec<Symbol> {
+        match self {
+            Exp::List(list) => list.iter().map(|e| e.as_symbol()).collect(),
+            _ => panic!("Expected list"),
         }
     }
 
@@ -67,33 +87,46 @@ impl Exp {
         }
     }
 
-    fn as_fn(&self) -> fn(List) -> Exp {
+    fn invoke(&self, env_tree: &mut EnvTree, args: List) -> Exp {
         match self {
-            Exp::Function(f) => *f,
+            Exp::Function(f) => f(env_tree, args),
+            Exp::Procedure(p) => p.invoke(env_tree, args),
             _ => panic!("Expected function!"),
         }
     }
 }
 
+pub type EnvId = DefaultKey;
+type EnvTree = SlotMap<EnvId, Env>;
+
 #[derive(Default)]
 struct Env {
-    outer: Arc<Mutex<Env>>,
+    outer: Option<EnvId>,
     symbols: HashMap<Symbol, Exp>,
 }
 
 impl Env {
-    pub fn new(parameters: Vec<Symbol>, arguments: List, outer: Arc<Mutex<Env>>) -> Self {
+    pub fn insert_into(
+        env_tree: &mut EnvTree,
+        parameters: Vec<Symbol>,
+        arguments: List,
+        outer: Option<EnvId>,
+    ) -> EnvId {
         let mut result = Self::default();
         result.symbols.extend(parameters.iter().cloned().zip(arguments.iter().cloned()));
         result.outer = outer;
-        result
+        env_tree.insert(result)
     }
 
     pub fn insert(&mut self, symbol: impl Into<String>, exp: Exp) {
         self.symbols.insert(symbol.into(), exp);
     }
 
-    pub fn insert_fn(&mut self, symbol: impl Into<String>, function: fn(List) -> Exp) {
+    pub fn insert_fn(
+        &mut self,
+        symbol: impl Into<String>,
+        function: fn(&mut EnvTree, List) -> Exp,
+    ) {
         self.insert(symbol, Exp::Function(function))
     }
 
@@ -101,36 +134,48 @@ impl Env {
         self.symbols.get(&symbol.into()).unwrap().clone()
     }
 
-    pub fn resolve(&self, symbol: impl Into<String>) -> Exp {
+    pub fn resolve(&self, env_tree: &EnvTree, symbol: impl Into<String>) -> Exp {
         let s = symbol.into();
         if self.symbols.contains_key(&s) {
             self.get(s)
+        } else if let Some(outer) = self.outer {
+            if let Some(e) = env_tree.get(outer) {
+                e.resolve(env_tree, s)
+            } else {
+                panic!("Env not found");
+            }
         } else {
-            self.outer.lock().unwrap().resolve(s)
+            panic!("Symbol not found {s}");
         }
     }
 
-    pub fn find_mut(&mut self, _symbol: impl Into<String>) -> Arc<Mutex<Env>> {
-        todo!("")
+    pub fn find(&self, env_tree: &EnvTree, symbol: impl Into<String>, current: EnvId) -> EnvId {
+        let s = symbol.into();
+        if self.symbols.contains_key(&s) {
+            current
+        } else if let Some(outer) = self.outer {
+            env_tree.get(outer).unwrap().find(env_tree, s, outer)
+        } else {
+            panic!("Symbol not found {s}");
+        }
     }
 }
 
+#[derive(Clone, Debug)]
 struct Procedure {
     pub parameters: Vec<Symbol>,
     pub body: Exp,
-    pub env: Arc<Mutex<Env>>,
+    pub env: EnvId,
 }
 
 impl Procedure {
-    pub fn new(parameters: Vec<Symbol>, body: Exp, env: Arc<Mutex<Env>>) -> Self {
+    pub fn new(parameters: Vec<Symbol>, body: Exp, env: EnvId) -> Self {
         Self { parameters, body, env }
     }
 
-    pub fn invoke(&self, arguments: List) {
-        eval(
-            self.body.clone(),
-            &mut Env::new(self.parameters.clone(), arguments, self.env.clone()),
-        );
+    pub fn invoke(&self, env_tree: &mut EnvTree, arguments: List) -> Exp {
+        let env_id = Env::insert_into(env_tree, self.parameters.clone(), arguments, Some(self.env));
+        eval(self.body.clone(), env_tree, env_id)
     }
 }
 
@@ -177,48 +222,73 @@ fn atom(token: String) -> Atom {
 
 fn standard_env() -> Env {
     let mut result = Env::default();
-    result.insert_fn("+", |list| Exp::num(list[0].as_number() + list[1].as_number()));
-    result.insert_fn("*", |list| Exp::num(list[0].as_number() * list[1].as_number()));
+    result.insert_fn("+", |_, list| Exp::num(list[0].as_number() + list[1].as_number()));
+    result.insert_fn("-", |_, list| Exp::num(list[0].as_number() - list[1].as_number()));
+    result.insert_fn("*", |_, list| Exp::num(list[0].as_number() * list[1].as_number()));
+    result.insert_fn("/", |_, list| Exp::num(list[0].as_number() / list[1].as_number()));
+    result.insert_fn("<=", |_, list| Exp::bool(list[0].as_number() <= list[1].as_number()));
+    result.insert_fn(">=", |_, list| Exp::bool(list[0].as_number() >= list[1].as_number()));
+    result.insert_fn("<", |_, list| Exp::bool(list[0].as_number() >= list[1].as_number()));
+    result.insert_fn(">", |_, list| Exp::bool(list[0].as_number() >= list[1].as_number()));
+    result.insert_fn("abs", |_, list| Exp::num(list[0].as_number().abs()));
+    result.insert_fn("append", |_, list| {
+        Exp::List(list.iter().flat_map(|x| x.as_exp_list()).collect())
+    });
+    result.insert_fn("apply", |env_tree, list| {
+        list[0].invoke(env_tree, list.iter().skip(1).cloned().collect())
+    });
+    result.insert_fn("begin", |_, list| list[list.len() - 1].clone());
+    result.insert_fn("car", |_, list| list[0].clone());
+    result.insert_fn("cdr", |_, list| Exp::List(list.iter().skip(1).cloned().collect()));
+    result.insert_fn("cons", |_, list| {
+        Exp::List(
+            iter::once(list[0].clone()).chain(list[1].as_exp_list().iter().cloned()).collect(),
+        )
+    });
     result.insert("pi", Exp::Atom(Atom::Number(consts::PI)));
-    result.insert_fn("begin", |list| list[list.len() - 1].clone());
     result
 }
 
-fn eval(x: Exp, env: &mut Env) -> Exp {
+fn eval(x: Exp, env_tree: &mut EnvTree, env_id: EnvId) -> Exp {
     match x {
-        Exp::Atom(Atom::Symbol(s)) => env.resolve(s),
+        Exp::Atom(Atom::Symbol(s)) => env_tree.get(env_id).unwrap().resolve(env_tree, s),
         Exp::Atom(Atom::Number(..)) => x,
         Exp::Atom(Atom::Bool(..)) => x,
         Exp::Function(..) => x,
+        Exp::Procedure(..) => x,
         Exp::List(list) if list.is_empty() => panic!("Cannot evaluate empty list"),
         Exp::List(list) if list[0].as_symbol() == *"quote" => list[1].clone(),
         Exp::List(list) if list[0].as_symbol() == *"if" => {
-            let result = if eval(list[1].clone(), env).as_bool() {
+            let result = if eval(list[1].clone(), env_tree, env_id).as_bool() {
                 list[2].clone()
             } else {
                 list[3].clone()
             };
-            eval(result, env)
+            eval(result, env_tree, env_id)
         }
         Exp::List(list) if list[0].as_symbol() == *"define" => {
-            let result = eval(list[2].clone(), env);
-            env.insert(list[1].as_symbol(), result.clone());
+            let result = eval(list[2].clone(), env_tree, env_id);
+            env_tree.get_mut(env_id).unwrap().insert(list[1].as_symbol(), result.clone());
             result
         }
         Exp::List(list) if list[0].as_symbol() == *"set!" => {
-            let _symbol = list[1].clone().as_symbol();
+            let symbol = list[1].clone().as_symbol();
             let exp = list[2].clone();
-
-            // env.find_mut(symbol.clone()).insert(symbol, evaluated.clone());
-            eval(exp, env)
+            let evaluated = eval(exp, env_tree, env_id);
+            let target_id = env_tree.get(env_id).unwrap().find(env_tree, symbol.clone(), env_id);
+            env_tree.get_mut(target_id).unwrap().insert(symbol, evaluated);
+            Exp::Atom(Atom::Bool(true))
         }
+        Exp::List(list) if list[0].as_symbol() == *"lambda" => Exp::Procedure(Box::new(
+            Procedure::new(list[1].as_symbol_list(), list[2].clone(), env_id),
+        )),
         Exp::List(list) => {
-            let proc = eval(list[0].clone(), env);
+            let proc = eval(list[0].clone(), env_tree, env_id);
             let mut args: List = vec![];
             for x in list.iter().skip(1) {
-                args.push(eval(x.clone(), env));
+                args.push(eval(x.clone(), env_tree, env_id));
             }
-            proc.as_fn()(args)
+            proc.invoke(env_tree, args)
         }
     }
 }
@@ -226,6 +296,8 @@ fn eval(x: Exp, env: &mut Env) -> Exp {
 pub fn run() {
     let mut line_editor = Reedline::create();
     let prompt = DefaultPrompt::default();
+    let mut env_tree = EnvTree::default();
+    let standard_env_id = env_tree.insert(standard_env());
 
     loop {
         let sig = line_editor.read_line(&prompt);
@@ -236,7 +308,7 @@ pub fn run() {
                 println!("Tokenized: {:?}", tokenized);
                 let parsed = read_from_tokens(&mut tokenized);
                 println!("Parsed: {:?}", parsed);
-                let eval = eval(parsed, &mut standard_env());
+                let eval = eval(parsed, &mut env_tree, standard_env_id);
                 println!("Result: {:?}", eval);
             }
             Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
